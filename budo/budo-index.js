91dc1a62319d84336d51066f62f9ac8c7c46efee
chris watts <chris@byhook.com>
2016-03-08 13:01:42 -0800

    refactoring app into proper app module

--- budo/budo-index.js
+++ budo/budo-index.js
@@ -1,45 +1,9 @@
 
 "use strict";
 
-console.log ("hello budo2");
 
+var App = require ("./app");
 
-
-
-var App = function () {
-
-
-  this.loadDC();
-
-
-
-};
-
-
-App.prototype.loadDC = function () {
-
-  if (!Enabler.isInitialized()) {
-    Enabler.addEventListener(studio.events.StudioEvent.INIT,this.enablerInitialized.bind (this));
-  } else {
-    enablerInitialized();
-  }
-
-
-};
-
-
-
-App.prototype.enablerInitialized = function () {
-
-console.log ("DC Ready");
-
-
-
-
-
-
-
-};
 
 
 
